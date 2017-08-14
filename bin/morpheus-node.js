b8965ef9f4e8b270cd4a57017274c37cb9cd08f1
Joshua Gould <jgould@broadinstitute.org>
2017-08-14 10:09:10 -0400

    use jsdom instead of phantomjs

---
+++ bin/morpheus-node.js
@@ -0,0 +1,74 @@
+exports.saveImage = function (options, outputFile, outputFormat) {
+  const path = require('path');
+  const fs = require('fs');
+  const jsdom = require('jsdom');
+  const dom = new jsdom.JSDOM('<div></div>');
+  const Canvas = require('canvas-prebuilt');
+  // globals
+  // XMLSerializer = require('xmlserializer')
+  $ = require('../node_modules/morpheus-app/js/jquery-2.2.4.min.js')(dom.window);
+  _ = require('../node_modules/morpheus-app/js/underscore-min.js');
+  d3 = require('../node_modules/morpheus-app/js/d3.min.js');
+  const morpheus = require('../node_modules/morpheus-app/js/morpheus-latest.min.js');
+  const nodeFetch = require('node-fetch');
+  Response = nodeFetch.Response;
+  Headers = nodeFetch.Headers;
+  Request = nodeFetch.Request;
+
+  fetch = function (url, options) {
+    const request = new Request(url, options);
+    if (request.url.substring(0, 5) === 'file:') {
+      return new Promise((resolve, reject) => {
+        const filePath = path.normalize(url.substring('file:///'.length));
+        if (!fs.existsSync(filePath)) {
+          reject(`File not found: ${filePath}`);
+        }
+        const readStream = fs.createReadStream(filePath);
+        readStream.on('open', function () {
+          resolve(new Response(readStream, {
+            url: request.url,
+            status: 200,
+            statusText: 'OK',
+            size: fs.statSync(filePath).size,
+            timeout: request.timeout
+          }));
+        });
+      });
+    } else {
+      return nodeFetch(url, options);
+    }
+  };
+
+  options.interactive = false;
+  options.error = function (err) {
+    console.log('Error creating heat map', err);
+    process.exit();
+  };
+  options.loadedCallback = function (heatMap) {
+    const bounds = heatMap.getTotalSize();
+    const height = bounds.height;
+    const width = bounds.width;
+    var canvas;
+    if (outputFormat === 'png') {
+      canvas = new Canvas(width, height);
+    } else {
+      canvas = new Canvas(width, height, outputFormat);
+    }
+    heatMap.snapshot(canvas.getContext('2d'));
+    fs.writeFile(outputFile, canvas.toBuffer(), function (err) {
+      if (err) {
+        console.log('error saving ' + outputFile, err);
+        process.exit(1);
+      }
+      process.exit();
+    });
+
+  };
+  new morpheus.HeatMap(options);
+};
+
+
+
+
+
+
--- bin/morpheus.js
+++ bin/morpheus.js
@@ -1,15 +1,12 @@
-#!/usr/bin/env node
-
 var commandLineArgs = require('command-line-args');
 var getUsage = require('command-line-usage');
 var path = require('path');
-var phantomjs = require('phantomjs-prebuilt');
+var fs = require('fs');
 var optionDefinitions = [
   {name: 'input', alias: 'i', type: String, description: 'JSON configuration (https://software.broadinstitute.org/morpheus/configuration.html)'},
   {name: 'output', alias: 'o', type: String, description: 'output image file'},
   {name: 'format', alias: 'f', type: String, description: 'output file format (png or svg)'},
-  {name: 'help', alias: 'h', type: Boolean, description: 'print this usage guide'},
-  {name: 'port', type: Number, description: 'web server port (default is 9222)', defaultValue: 9222}
+  {name: 'help', alias: 'h', type: Boolean, description: 'print this usage guide'}
 ];
 
 var commandArgs;
@@ -47,21 +44,65 @@ if (commandArgs.output == null) {
 }
 if (commandArgs.format == null) {
   // infer format from file extension
-  if (endsWith(commandArgs.output.toLowerCase(), '.png')) {
+  var formats = ['png', 'svg', 'pdf'];
+  for (var i = 0; i < formats.length; i++) {
+    if (endsWith(commandArgs.output.toLowerCase(), '.' + formats[i])) {
+      commandArgs.format = formats[i];
+      break;
+    }
+  }
+  if (commandArgs.format == null) {
     commandArgs.format = 'png';
-  } else {
-    commandArgs.format = 'svg';
   }
 }
 if (!endsWith(commandArgs.output.toLowerCase(), '.' + commandArgs.format)) {
   commandArgs.output += '.' + commandArgs.format;
 }
-var program = phantomjs.exec('--web-security=no', path.join(__dirname + path.sep + 'morpheus-phantom.js'), path.normalize(__dirname + path.sep + '..'), commandArgs.input,
-    commandArgs.output,
-    commandArgs.format,
-    commandArgs.port);
-program.stdout.pipe(process.stdout);
-program.stderr.pipe(process.stderr);
+
+function convertFilePathToUrl(value) {
+  if (value != null) {
+    value = path.normalize(value);
+    if (fs.existsSync(value)) {
+      var pathName = path.resolve(value).replace(/\\/g, '/');
+      // Windows drive letter must be prefixed with a slash
+      if (pathName[0] !== '/') {
+        pathName = '/' + pathName;
+      }
+      return encodeURI('file://' + pathName);
+    }
+  }
+  return value;
+};
+
+var options;
+if (fs.existsSync(path.normalize(commandArgs.input))) {
+  var content = fs.readFileSync(commandArgs.input, {encoding: 'UTF-8'}).trim();
+  options = JSON.parse(content);
+} else {
+  options = JSON.parse(commandArgs.input.trim());
+}
+options.dataset = convertFilePathToUrl(options.dataset);
+options.rowDendrogram = convertFilePathToUrl(options.rowDendrogram);
+options.columnDendrogram = convertFilePathToUrl(options.columnDendrogram);
+if (options.columnAnnotations) {
+  for (var i = 0; i < options.columnAnnotations.length; i++) {
+    options.columnAnnotations[i].file = convertFilePathToUrl(options.columnAnnotations[i].file);
+  }
+}
+if (options.rowAnnotations) {
+  for (var i = 0; i < options.rowAnnotations.length; i++) {
+    options.rowAnnotations[i].file = convertFilePathToUrl(options.rowAnnotations[i].file);
+  }
+}
+var morpheusNode = require('./morpheus-node.js');
+morpheusNode.saveImage(options, commandArgs.output, commandArgs.format);
+
+// var program = phantomjs.exec('--web-security=no', path.join(__dirname + path.sep + 'morpheus-phantom.js'), path.normalize(__dirname + path.sep + '..'), commandArgs.input,
+//     commandArgs.output,
+//     commandArgs.format,
+//     commandArgs.port);
+// program.stdout.pipe(process.stdout);
+// program.stderr.pipe(process.stderr);
 
 
 
